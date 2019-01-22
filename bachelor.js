2ae09372d55362ef5876bbb775a482b6a601c0f3
Calie Brennan <calie.brennan@gmail.com>
2019-01-21 23:03:44 -0500

    updates
    updates

--- bachelor.js
+++ bachelor.js
@@ -17,17 +17,17 @@ var alexB = new Contestant(0, "Alex B", 30, 27, 0, 17, 0, true, 1
 );
 var caitlin = new Contestant(1, "Caitlin", 29, 29, 25, 14, 0, true, 2
 );
-var hannahB = new Contestant(2, "Hannah B", 28, 30, 26, 30, 30, true, 2
+var hannahB = new Contestant(2, "Hannah B", 28, 30, 26, 30, 30, true, 3
 );
-var cassie = new Contestant(3, "Cassie", 27, 24, 22, 24, 29, true, 2
+var cassie = new Contestant(3, "Cassie", 27, 24, 22, 24, 29, true, 3
 );
-var caelynn = new Contestant(4, "Caelynn", 26, 25, 28, 28, 28, true, 2
+var caelynn = new Contestant(4, "Caelynn", 26, 25, 28, 28, 28, true, 3
 );
-var demi = new Contestant(5, "Demi", 25, 21, 23, 20, 18, true, 2
+var demi = new Contestant(5, "Demi", 25, 21, 23, 20, 18, true, 3
 );
 var laura = new Contestant(6, "Laura", 24, 16, 29, 11, 27, false, 0
 );
-var hannahG = new Contestant(7, "Hannah G", 23, 18, 15, 27, 26, true, 2
+var hannahG = new Contestant(7, "Hannah G", 23, 18, 15, 27, 26, true, 3
 );
 var bri = new Contestant(8, "Bri", 22, 23, 27, 22, 12, true, 2
 );
@@ -43,35 +43,35 @@ var annie = new Contestant(13, "Annie", 17, 26, 0, 29, 0, true, 1
 );
 var nina = new Contestant(14, "Nina", 16, 7, 17, 21, 22, true, 2
 );
-var kirpa = new Contestant(15, "Kirpa", 15, 9, 18, 3, 11, true, 2
+var kirpa = new Contestant(15, "Kirpa", 15, 9, 18, 3, 11, true, 3
 );
-var tracy = new Contestant(16, "Tracy", 14, 2, 19, 7, 0, true, 2
+var tracy = new Contestant(16, "Tracy", 14, 2, 19, 7, 0, true, 3
 );
-var sydney = new Contestant(17, "Sydney", 13, 15, 9, 8, 25, true, 2
+var sydney = new Contestant(17, "Sydney", 13, 15, 9, 8, 25, true, 3
 );
-var nicole = new Contestant(18, "Nicole", 12, 10, 14, 9, 21, true, 2
+var nicole = new Contestant(18, "Nicole", 12, 10, 14, 9, 21, true, 3
 );
-var heather = new Contestant(19, "Heather", 11, 11, 0, 5, 8, true, 2
+var heather = new Contestant(19, "Heather", 11, 11, 0, 5, 8, true, 3
 );
 var erika = new Contestant(20, "Erika", 10, 17, 21, 15, 20, true, 1
 );
-var elyse = new Contestant(21, "Elyse", 9, 12, 13, 23, 19, true, 2
+var elyse = new Contestant(21, "Elyse", 9, 12, 13, 23, 19, true, 3
 );
-var tayshia = new Contestant(22, "Tayshia", 8, 4, 10, 26, 13,true, 2
+var tayshia = new Contestant(22, "Tayshia", 8, 4, 10, 26, 13,true, 3
 );
 var revian = new Contestant(23, "Revian", 7, 3, 16, 2, 15, false, 0
 );
 var angelique = new Contestant(24, "Angelique", 6, 1, 0, 1, 7, true,1 
 );
-var court = new Contestant(25, "Courtney", 5, 14, 11, 13, 7, false, 2
+var court = new Contestant(25, "Courtney", 5, 14, 11, 13, 7, false, 3
 );
 var devin = new Contestant(26, "Devin", 4, 19, 8, 12, 17, false, 0
 );
 var tahzjuan = new Contestant(27, "Tahjuan", 3, 5, 0, 4, 9, false, 0
 );
-var katie = new Contestant(28, "Katie", 2, 8, 0, 10, 10, true, 2
+var katie = new Contestant(28, "Katie", 2, 8, 0, 10, 10, true, 3
 );
-var onyeka = new Contestant(29, "Onyeka", 1, 6, 0, 19, 14, true, 2
+var onyeka = new Contestant(29, "Onyeka", 1, 6, 0, 19, 14, true, 3
 );
 
 
@@ -107,7 +107,7 @@ var onyeka = new Contestant(29, "Onyeka", 1, 6, 0, 19, 14, true, 2
     tayshia,
     tracy);
 
-    //console.log(contestantsArray);
+    //console.log(contestantsArray[0]);
 
   //console.log(contestantsArray[1].calie);
 
@@ -169,7 +169,13 @@ function createCalieList(){
             return arr.calie === i+1;
         }
         var list = contestantsArray.find(isI); 
-        html += '<tr><td>'+list.first + '</td><td>' + list.calie + '</td></tr>';
+        if(list.weeksactive < 3){
+            html += '<tr style="color:red"><td>'+list.first + '</td><td>' + list.calie + '</td></tr>';
+        }
+        else{
+            html += '<tr ><td>'+list.first + '</td><td>' + list.calie + '</td></tr>';
+        }
+        
     }
     document.getElementById('caliesDOMlist').innerHTML+= html;
 }
@@ -181,12 +187,58 @@ function createVanessaList(){
             return arr.vanessa === i+1;
         }
         var list = contestantsArray.find(isI); 
-        html += '<tr><td>'+caliesList.first + '</td><td>' + caliesList.calie + '</td></tr>';
+        console.log(list);
+        if(list.weeksactive < 3){
+            html += '<tr style="color:red"><td>'+list.first + '</td><td>' + list.vanessa + '</td></tr>';
+        }
+        else{
+            html += '<tr ><td>'+list.first + '</td><td>' + list.vanessa + '</td></tr>';
+        }
     }
-    document.getElementById('vanessasDOMlist').innerHTML+= html;
+    document.getElementById('vanessaDOMlist').innerHTML+= html;
 }
 
+function createJamesList(){
+    var html;
+    for(i=29; i <= 29 && i >= 0; i--){
+        function isI(arr) { 
+            return arr.james === i+1;
+        }
+        var list = contestantsArray.find(isI); 
+        console.log(list);
+        if(list.weeksactive < 3){
+            html += '<tr style="color:red"><td>'+list.first + '</td><td>' + list.james + '</td></tr>';
+        }
+        else{
+            html += '<tr><td>'+list.first + '</td><td>' + list.james + '</td></tr>';
+        }
+    }
+    document.getElementById('jamesDOMlist').innerHTML+= html;
+}
+
+function createJaviList(){
+    var html;
+    for(i=29; i <= 29 && i >= 0; i--){
+        function isI(arr) { 
+            return arr.javi === i+1;
+        }
+        var list = contestantsArray.find(isI); 
+        console.log(list);
+        if(list.weeksactive < 3){
+            html += '<tr style="color:red"><td>'+list.first + '</td><td>' + list.james + '</td></tr>';
+        }
+        else{
+            html += '<tr><td>'+list.first + '</td><td>' + list.javi + '</td></tr>';
+        }
+    }
+    document.getElementById('javiDOMlist').innerHTML+= html;
+}
+
+
 //createCalieList();
+//createVanessaList();
+//createJamesList();
+//createJaviList();
 
 document.getElementById('caliescore').innerHTML = calculateCalieScore();
 document.getElementById('jamesscore').innerHTML = calculateJamesScore();
